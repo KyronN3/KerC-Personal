fc01ea71368e0a76f5892014681638fc68688b8f
kyron <jameskyronsostino36@gmail.com>
2025-05-05 00:35:23 +0800

    Patch 7.6.1

--- src/ProfilePage/ProfilePageAdmin.jsx
+++ src/ProfilePage/ProfilePageAdmin.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import Style from './ProfilePageAdmin.module.css';
 import Logo from '../assets/imgs/logo.png';
 import { useState, useEffect, useContext } from 'react';
